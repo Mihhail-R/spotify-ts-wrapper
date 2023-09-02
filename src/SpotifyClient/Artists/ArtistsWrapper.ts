81d43d25cbbac002210c2433eab6d12d662dd43f
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-02 21:15:29 +0300

    Add Spotify ArtistsWrapper

---
+++ src/SpotifyClient/Artists/ArtistsWrapper.ts
@@ -0,0 +1,49 @@
+import { PaginatedAlbums } from '../../Types/Album';
+import { Artist } from '../../Types/Artist';
+import { Track } from '../../Types/Track';
+import IHttpClient from '../IHttpClient';
+
+export default class ArtistsWrapper {
+	private readonly path: string = 'artists';
+	constructor(private readonly client: IHttpClient) {
+		this.client = client;
+	}
+	public async getArtist(artistId: string): Promise<Artist> {
+		return await this.client.sendGetRequest<Artist>(
+			`${this.path}/${artistId}`
+		);
+	}
+
+	public async getArtists(
+		artistIds: string[]
+	): Promise<{ artists: Artist[] }> {
+		return await this.client.sendGetRequest<{ artists: Artist[] }>(
+			`${this.path}`,
+			{
+				ids: artistIds.join(','),
+			}
+		);
+	}
+
+	public async getArtistAlbums(artistId: string): Promise<PaginatedAlbums> {
+		return await this.client.sendGetRequest<PaginatedAlbums>(
+			`${this.path}/${artistId}/albums`
+		);
+	}
+
+	public async getArtistTopTracks(
+		artistId: string
+	): Promise<{ tracks: Track[] }> {
+		return await this.client.sendGetRequest<{ tracks: Track[] }>(
+			`${this.path}/${artistId}/top-tracks`
+		);
+	}
+
+	public async getArtistRelatedArtists(
+		artistId: string
+	): Promise<{ artists: Artist[] }> {
+		return await this.client.sendGetRequest<{ artists: Artist[] }>(
+			`${this.path}/${artistId}/related-artists`
+		);
+	}
+}
