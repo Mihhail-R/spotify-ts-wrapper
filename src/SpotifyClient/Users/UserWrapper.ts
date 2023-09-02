8e963ccfd110ad2416f8de7ff3b6521887462aae
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-02 21:18:59 +0300

    Add Spotify UserWrapper

---
+++ src/SpotifyClient/Users/UserWrapper.ts
@@ -0,0 +1,75 @@
+import { PaginatedArtists } from '../../Types/Artist';
+import { Me, UserTopItems, UserTopItemsTypes } from '../../Types/User';
+import IHttpClient from '../IHttpClient';
+
+export default class UserWrapper {
+	constructor(private readonly client: IHttpClient) {
+		this.client = client;
+	}
+	public async me(): Promise<Me> {
+		return await this.client.sendGetRequest<Me>('me');
+	}
+
+	public async getTopItems<T extends UserTopItemsTypes>(
+		type: UserTopItemsTypes = 'artists',
+		limit: number = 20,
+		offset: number = 0,
+		timeRange: 'long_term' | 'medium_term' | 'short_term' = 'medium_term'
+	): Promise<UserTopItems<T>> {
+		if (type === 'artists') {
+			return await this.client.sendGetRequest<UserTopItems<T>>(
+				`me/top/${type}?limit=${limit}&offset=${offset}&time_range=${timeRange}`
+			);
+		}
+
+		return await this.client.sendGetRequest<UserTopItems<T>>(
+			`me/top/${type}?limit=${limit}&offset=${offset}&time_range=${timeRange}`
+		);
+	}
+
+	public async followPlaylist(
+		playlistId: string,
+		publicPlaylist: boolean = true
+	): Promise<void> {
+		return await this.client.sendPutRequest(
+			`playlists/${playlistId}/followers`,
+			{ public: publicPlaylist }
+		);
+	}
+
+	public async unfollowPlaylist(playlistId: string): Promise<void> {
+		return await this.client.sendDeleteRequest(
+			`playlists/${playlistId}/followers`
+		);
+	}
+
+	public async followArtist(artistId: string): Promise<void> {
+		return await this.client.sendPutRequest(
+			`me/following?type=artist&ids=${artistId}`
+		);
+	}
+
+	public async unfollowArtist(artistId: string): Promise<void> {
+		return await this.client.sendDeleteRequest(
+			`me/following?type=artist&ids=${artistId}`
+		);
+	}
+
+	public async followedArtists(
+		limit: number = 20,
+		after?: string
+	): Promise<{ artists: PaginatedArtists }> {
+		return await this.client.sendGetRequest<{ artists: PaginatedArtists }>(
+			`me/following?type=artist&limit=${limit}&after=${after}`
+		);
+	}
+
+	public async isFollowing(
+		type: 'artist' | 'user',
+		ids: string[]
+	): Promise<boolean[]> {
+		return await this.client.sendGetRequest<boolean[]>(
+			`me/following/contains?type=${type}&ids=${ids.join(',')}`
+		);
+	}
+}
