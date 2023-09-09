080899459bebb8dfdb66cd9e666749d8a740ec5f
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-09 20:49:37 +0300

    Add track recommendation types

--- src/Types/Track.ts
+++ src/Types/Track.ts
@@ -178,3 +178,71 @@ export type TrackItems = {
   total: number;
   items: Track[];
 };
+
+export type RecommendationSeed = {
+  afterFilteringSize: number;
+  afterRelinkingSize: number;
+  href: string;
+  id: string;
+  initialPoolSize: number;
+  type: string;
+};
+
+export type TrackRecommendations = {
+  seeds: RecommendationSeed[];
+  tracks: Track[];
+};
+
+/**
+ * @see https://developer.spotify.com/documentation/web-api/reference/get-recommendations
+ */
+export interface TrackRecommendationInput {
+  limit?: number;
+  market?: string;
+  seed_artists: string[];
+  seed_genres: string[];
+  seed_tracks: string[];
+  // range from 0.0 to 1.0
+  min_acousticness?: number;
+  max_acousticness?: number;
+  target_acousticness?: number;
+  min_danceability?: number;
+  max_danceability?: number;
+  target_danceability?: number;
+  min_duration_ms?: number;
+  max_duration_ms?: number;
+  target_duration_ms?: number;
+  min_energy?: number;
+  max_energy?: number;
+  target_energy?: number;
+  min_instrumentalness?: number;
+  max_instrumentalness?: number;
+  target_instrumentalness?: number;
+  min_key?: number;
+  max_key?: number;
+  target_key?: number;
+  min_liveness?: number;
+  max_liveness?: number;
+  target_liveness?: number;
+  min_loudness?: number;
+  max_loudness?: number;
+  target_loudness?: number;
+  min_mode?: number;
+  max_mode?: number;
+  target_mode?: number;
+  min_popularity?: number;
+  max_popularity?: number;
+  target_popularity?: number;
+  min_speechiness?: number;
+  max_speechiness?: number;
+  target_speechiness?: number;
+  min_tempo?: number;
+  max_tempo?: number;
+  target_tempo?: number;
+  min_time_signature?: number;
+  max_time_signature?: number;
+  target_time_signature?: number;
+  min_valence?: number;
+  max_valence?: number;
+  target_valence?: number;
+}
