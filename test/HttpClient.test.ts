3d226ae9cde0e328f6390f33d5ff33a343d5d127
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-10 23:13:17 +0300

    Fix HttpClient tests

--- test/HttpClient.test.ts
+++ test/HttpClient.test.ts
@@ -21,54 +21,54 @@ describe('HttpClient', () => {
 	});
 
 	it('Should send a post request', async () => {
-		const result = await httpClient.post("posts", {
+		const result = httpClient.post("posts", {
 			title: "foo",
 			body: "bar",
 			userId: 1,
 		});
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 
 	it('Should send a post request with params', async () => {
-		const result = await httpClient.post("posts/1", {
+		const result = httpClient.post("posts/1", {
 			title: "foo",
 			body: "bar",
 			userId: 1,
 		}, {userId: 1});
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 
 	it('Should send a put request', async () => {
-		const result = await httpClient.put("posts/1", {
+		const result = httpClient.put("posts/1", {
 			title: "foo",
 			body: "bar",
 			userId: 1,
 		});
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 
 	it('Should send a put request with params', async () => {
-		const result = await httpClient.put("posts/1", {
+		const result = httpClient.put("posts/1", {
 			title: "foo",
 			body: "bar",
 			userId: 1,
 		}, {userId: 1});
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 
 	it('Should send a delete request', async () => {
-		const result = await httpClient.delete("posts/1");
+		const result = httpClient.delete("posts/1");
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 
 	it('Should send a delete request with params', async () => {
-		const result = await httpClient.delete("posts/1", {userId: 1});
+		const result = httpClient.delete("posts/1", {userId: 1});
 
-		expect(result).toBeUndefined();
+		await expect(result).resolves.not.toThrow();
 	});
 });
