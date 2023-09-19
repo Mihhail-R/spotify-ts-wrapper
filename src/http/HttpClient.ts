bf2ac2db87a5e3ea1d6a4d21b1204fbfe51091a8
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-19 22:39:07 +0300

    Add user authentication flow

--- src/http/HttpClient.ts
+++ src/http/HttpClient.ts
@@ -1,7 +1,11 @@
 import { fetch } from "undici";
 
 import IHttpClient from "../SpotifyClient/IHttpClient";
-import { ClientCredentials } from "../Types/ClientCredentials";
+import {
+  ClientCredentials,
+  UserClientCredentials,
+  UserScopes,
+} from "../Types/ClientCredentials";
 
 import { ClientConfigurations } from "./ClientConfig";
 
@@ -29,6 +33,37 @@ export default class HttpClient implements IHttpClient {
     }
   }
 
+  private createUrl(endpoint: string): URL {
+    return new URL(endpoint, this.baseUrl);
+  }
+
+  private parseParams(params: any): URLSearchParams {
+    const urlSearchParams = new URLSearchParams();
+
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        if (params[key]) {
+          return urlSearchParams.append(key, params[key]);
+        }
+      });
+    }
+
+    return urlSearchParams;
+  }
+
+  private generateRandomString(length: number): string {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
   /**
    * Authorize app scope with client credentials
    * Ref: https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
@@ -54,6 +89,10 @@ export default class HttpClient implements IHttpClient {
         body: "grant_type=client_credentials",
       });
 
+      if (result.status !== 200) {
+        throw new Error("Failed to authorize app");
+      }
+
       const clientCredentials = (await result.json()) as ClientCredentials;
 
       this.accessToken = clientCredentials.access_token;
@@ -62,22 +101,87 @@ export default class HttpClient implements IHttpClient {
     }
   }
 
-  private createUrl(endpoint: string): URL {
-    return new URL(endpoint, this.baseUrl);
-  }
-
-  private parseParams(params: any): URLSearchParams {
-    const urlSearchParams = new URLSearchParams();
-
-    if (params) {
-      Object.keys(params).forEach((key) => {
-        if (params[key]) {
-          return urlSearchParams.append(key, params[key]);
-        }
-      });
+  /**
+   * Create user authentication url with client credentials
+   * Use this url to redirect user to spotify login page
+   * @param redirectUri
+   * @param scopes
+   */
+  public createUserAuthUrl(redirectUri: string, scopes: UserScopes[]): string {
+    if (!this.clientId) {
+      throw new Error("Client id is not defined");
     }
 
-    return urlSearchParams;
+    if (!this.authenticationUrl) {
+      throw new Error("Authentication url is not defined");
+    }
+
+    let url = "https://accounts.spotify.com/authorize";
+
+    const urlSearchParams = new URLSearchParams();
+    const state = this.generateRandomString(16);
+
+    urlSearchParams.append("client_id", this.clientId);
+    urlSearchParams.append("response_type", "code");
+    urlSearchParams.append("redirect_uri", redirectUri);
+    urlSearchParams.append("scope", scopes.join(" "));
+    urlSearchParams.append("state", state);
+
+    url += `?${urlSearchParams.toString()}`;
+
+    return url;
+  }
+
+  /**
+   * Authorize user scope with client credentials
+   * Returns UserClientCredentials, save access_token and refresh_token securely for later use
+   * @param code
+   * @param redirectUri
+   */
+  public async authorizeUser(
+    redirectUri: string,
+    code: string,
+  ): Promise<UserClientCredentials> {
+    if (!this.clientId) {
+      throw new Error("Client id is not defined");
+    }
+
+    if (!this.clientSecret) {
+      throw new Error("Client secret is not defined");
+    }
+
+    if (!this.authenticationUrl) {
+      throw new Error("Authentication url is not defined");
+    }
+
+    try {
+      const response = await fetch(this.authenticationUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${Buffer.from(
+            `${this.clientId}:${this.clientSecret}`,
+          ).toString("base64")}`,
+        },
+        body: new URLSearchParams({
+          code,
+          grant_type: "authorization_code",
+          redirect_uri: redirectUri,
+        }),
+      });
+
+      const data = (await response.json()) as UserClientCredentials;
+
+      if (!data.access_token && data.error) {
+        throw new Error(`${data.error}: ${data.error_description}`);
+      }
+
+      this.accessToken = data.access_token;
+
+      return data;
+    } catch (e) {
+      throw e;
+    }
   }
 
   private async genericRequest<T>(
