1e7580729f382fa0bfd48547db707ec24f07b73a
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-09-19 22:38:54 +0300

    Add UserClientCredentials and UserScopes types

--- src/Types/ClientCredentials.ts
+++ src/Types/ClientCredentials.ts
@@ -3,3 +3,33 @@ export type ClientCredentials = {
   token_type: string;
   expires_in: number;
 };
+
+export type UserClientCredentials = {
+  refresh_token?: string;
+  access_token?: string;
+  token_type?: string;
+  expires_in?: number;
+  error?: string;
+  error_description?: string;
+};
+
+export type UserScopes =
+  | "ugc-image-upload"
+  | "user-read-recently-played"
+  | "user-modify-playback-state"
+  | "user-read-playback-state"
+  | "app-remote-control"
+  | "streaming"
+  | "playlist-modify-public"
+  | "playlist-modify-private"
+  | "playlist-read-private"
+  | "playlist-read-collaborative"
+  | "user-follow-modify"
+  | "user-follow-read"
+  | "user-read-playback-position"
+  | "user-top-read"
+  | "user-library-modify"
+  | "user-library-read"
+  | "user-read-email"
+  | "user-read-private"
+  | "user-read-currently-playing";
